ae4f902f80e58fed9ac0263ca9f54b37fd1af688
alita <1906321518@qq.com>
2019-07-06 14:34:43 +0800

    update flask02

--- flask_class02/app.py
+++ flask_class02/app.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 # @Time    : 2019/7/3 10:06
 # @Author  : icon
-# @File    : app.py
+# @File    : Class_View.py
 import os
 
 from flask import Flask, render_template, request
@@ -13,6 +13,12 @@ app = Flask(
     static_folder='static'
 )
 back_content = []
+ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg', 'gif']
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
@@ -20,22 +26,36 @@ def index():
     return render_template('demo.html')
 
 
-@app.route('/upload')
-def upload_file():
-    return render_template('upload.html')
+# @app.route('/upload')
+# def upload_file():
+#     return render_template('upload.html')
 
 
-@app.route('/back_content', methods=['GET', 'POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file2():
+    if request.method == "GET":
+        return render_template('upload.html')
     if request.method == 'POST':
-        f = request.files['file']
+        f = request.files.get('file')
+        if not (f and allowed_file(f.filename)):
+            return f'图片格式不支持{f.filename.split(".")[1]}'
+        if f.filename.__contains__(' '):
+            return '不支持文件名包含空格'
+        if f.read().__len__() > 10 * 1024 * 1024 * 8:  # 10M
+            return '文件大小不能超过10M'
         if not f:
             return render_template('upload.html')
-        path = os.path.abspath(os.path.dirname(__file__) + '/static/')
-        f.save(path + f.filename)
+        f.seek(0)
+        path = os.path.abspath(os.getcwd()) + '/static/'
         for dir, folder, filename in os.walk(path):
             for fn in filename:
                 back_content.append(fn)
+        i = 1
+        while f.filename in back_content:
+            f.filename = f.filename.rsplit(".", 1)[0] + str(i) + '.' + f.filename.rsplit(".", 1)[1]
+            i += 1
+        f.save(path + f.filename)
+        back_content.append(f.filename)
         bc = sorted(list(set(back_content)))
         return render_template('back_content.html', back_content=bc)
 

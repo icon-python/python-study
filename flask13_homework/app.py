fff71cc69378d5d2467f52f506c20d88249fbdde
alita <1906321518@qq.com>
2019-08-01 13:12:54 +0800

    12 13

--- flask13_homework/app.py
+++ flask13_homework/app.py
@@ -12,18 +12,22 @@
 2、首页添加一条微博功能，字数少于 140 字。作者名字长度少于10字，创建时间为服务器时间戳，数据通过 flask-sqlalchemy 存储到数据库。
 3、用户注册和登录功能。（注册的密码先用明文存储），只有登录了才能发布新微博。
 4、如果用户未登录，首页只显示最近 5 条信息。
+5.实现 user 表的关注和粉丝功能：
+    1、进入用户页面，显示用户的名称，粉丝数量和关注者数量
+    2、 点击粉丝数可以查看所有粉丝的名字。
+    3、点击关注者数量可以查看所有关注者的数量。
 """
 
 from _datetime import datetime
 import time
-from flask import Flask, render_template, request, url_for, redirect, abort, session, flash
-from test.flask12_homework.forms import AddForm, LoginForm, RegisterForm
+from flask import Flask, render_template, request, url_for, redirect, abort, session, flash, jsonify
+from test.flask13_homework.forms import AddForm, LoginForm, RegisterForm
 
-# from class_dev.flask12_homework.models import *
 app = Flask(__name__)
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 import os
+
 app.secret_key = os.getenv("SECRET KEY", 'ss')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/demo'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -31,10 +35,19 @@ db = SQLAlchemy(app)  # 数据库对象
 migrate = Migrate(app, db)
 
 
+class Follow(db.Model):
+    fans_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    followers_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     phone = db.Column(db.String(11), unique=True)
-    pwd = db.Column(db.String(20), unique=True)
+    pwd = db.Column(db.String(20))
+    fanses = db.relationship('Follow', foreign_keys=[Follow.followers_id],
+                             backref=db.backref('followers', lazy='joined'), lazy='dynamic')
+    followers = db.relationship('Follow', foreign_keys=[Follow.fans_id], backref=db.backref('fans', lazy='joined'),
+                                lazy='dynamic')
 
 
 class Blog(db.Model):
@@ -45,8 +58,27 @@ class Blog(db.Model):
     content = db.Column(db.String(140))
 
 
-def create_all():
-    db.create_all()
+@app.route('/get_fans_followers', methods=["GET", "POST"])
+def get_fans_followers():
+    phone = session.get('phone')
+    if request.method == "GET":
+        if phone:
+            user = User.query.filter_by(phone=phone).first_or_404()
+            fans = [f.fans_id for f in user.fanses]
+            followers = [f.followers_id for f in user.followers]
+            return jsonify({'fans': fans, 'followers': followers})
+        else:
+            return jsonify({'fans': 0, 'followers': 0})
+    else:
+        if phone:
+            user = User.query.filter_by(phone=phone).first_or_404()
+            fans = [f.fans_id for f in user.fanses]
+            followers = [f.followers_id for f in user.followers]
+            fans_phone = [User.query.filter_by(id=fid).first_or_404().phone for fid in fans]
+            followers_phone = [User.query.filter_by(id=fid).first_or_404().phone for fid in followers]
+            return jsonify({'fans': fans_phone, 'followers': followers_phone})
+        else:
+            return jsonify({'fans': 0, 'followers': 0})
 
 
 def decorator_login(func):
@@ -64,7 +96,7 @@ def decorator_login(func):
 @app.route('/login', methods=["GET", "POST"], endpoint='login')
 def login():
     if request.method == "GET":
-        return render_template('index.html', login_form=LoginForm())
+        return render_template('login.html', login_form=LoginForm())
     else:
         login_form = LoginForm(request.form)
         phone = request.form.get('phone')
@@ -77,9 +109,7 @@ def login():
         else:
             if not phone_db:
                 flash('该手机号尚未注册')
-                # return redirect(url_for('login'))
-            return render_template('index.html', login_form=login_form)
-            # return str(login_form.errors)
+            return render_template('login.html', login_form=login_form)
 
 
 @app.route('/register', methods=["GET", "POST"], endpoint='register')
@@ -91,11 +121,12 @@ def register():
         if register_form.validate_on_submit():
             phone = request.form.get('phone')
             pwd = request.form.get('pwd')
-            create_all()
             new_user = User(phone=phone, pwd=pwd)
             db.session.add(new_user)
             db.session.commit()
             return redirect(url_for('login'))
+        else:
+            return render_template('register.html', register_form=register_form)
 
 
 @app.route('/blog', methods=["GET", "POST"], endpoint='blog')
@@ -103,9 +134,10 @@ def blog():
     if request.method == "GET":
         if 'phone' in session:
             blogs = Blog.query.order_by(Blog.create_time.desc()).limit(20).all()
+            return render_template("blog.html", blogs=blogs, phone=session['phone'])
         else:
             blogs = Blog.query.order_by(Blog.create_time.desc()).limit(5).all()
-        return render_template("blog.html", blogs=blogs)
+            return render_template("blog.html", blogs=blogs, phone=0)
     if request.method == "POST":
         title = request.form.get('search')
         b = Blog.query.filter_by(title=title).first_or_404()
@@ -133,7 +165,8 @@ def blog_add():
         form = AddForm(request.form)
         if form.validate_on_submit():
             create_time = str(datetime.fromtimestamp(int(time.time())))
-            new_blog = Blog(title=form.title.data, author=form.author.data, content=form.content.data, create_time=create_time)
+            new_blog = Blog(title=form.title.data, author=form.author.data, content=form.content.data,
+                            create_time=create_time)
             db.session.add(new_blog)
             db.session.commit()
             b = Blog.query.filter_by(create_time=create_time).first()
@@ -162,5 +195,5 @@ def add_ctc():
 
 
 if __name__ == '__main__':
-    # app.run(debug=False, host='0.0.0.0')
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
+    # app.run(debug=True)
---
+++ flask13_homework/models.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/7/28 15:21
+# @Author  : icon
+# @File    : models.py
+import os
+
+from test.flask13_homework.app import app
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy(app)  # 数据库对象
+migrate = Migrate(app, db)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    phone = db.Column(db.String(11), unique=True)
+    pwd = db.Column(db.String(20), unique=True)
+
+
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(20))
+    create_time = db.Column(db.String(20))
+    author = db.Column(db.String(10))
+    content = db.Column(db.String(140))
+
+
+def creat_all():
+    db.create_all()
+
+
+# blogs = Blog.query.order_by(Blog.create_time.desc()).limit(20).all()
+# print(blogs)
